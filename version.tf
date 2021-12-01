5656a0e0a95795fa678d5a067be4c7c64d286b0e
software1tkh <47729687+software1tkh@users.noreply.github.com>
2021-12-01 10:56:17 +0800

    update

--- version.tf
+++ version.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     example = {
-      version = "~> 1.0.2"
-      //source  = "software1tkh/example"
-      source  = "terraform-example.com/exampleprovider2/example"
+      version = "1.0.6"
+      source  = "software1tkh/example"
     }
   }
 }
